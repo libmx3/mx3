3be37484bb2ad4d7543f07398d4a76d54076d474
Piet Brauer <piet@nerdishbynature.com>
2014-05-19 19:35:38 +0200

    Add barebone ios xcode project

---
+++ example_ios/MX3/MX3/MXAppDelegate.m
@@ -0,0 +1,13 @@
+#import "MXAppDelegate.h"
+
+@implementation MXAppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+
+@end
---
+++ example_ios/MX3/MX3Tests/MX3Tests.m
@@ -0,0 +1,9 @@
+#import <XCTest/XCTest.h>
+
+@interface MX3Tests : XCTestCase
+
+@end
+
+@implementation MX3Tests
+
+@end
