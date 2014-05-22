d45ff7d32ecdd26f2a7902efb2aba722ea8e037e
Piet Brauer <piet@nerdishbynature.com>
2014-05-22 21:17:55 +0200

    Some style cleanup also use #import over #include

--- objc/MX3Api.h
+++ objc/MX3Api.h
@@ -1,12 +1,14 @@
-#include <Foundation/Foundation.h>
-#include "MX3Snapshot.h"
+#import <Foundation/Foundation.h>
+#import "MX3Snapshot.h"
 
 // a small proxy objc implementation for mx3::Api
 @interface MX3Api : NSObject
-- (instancetype)initWithRootPath:(NSString *)path;
+
+- (instancetype) initWithRootPath:(NSString *)path;
 - (void) dealloc;
 - (BOOL) hasUser;
 - (MX3Snapshot *) launches;
 - (NSString *) username;
 - (void) setUsername:(NSString *) username;
+
 @end
