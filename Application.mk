81b3fbe3bcbaa0d54f3d84182f6f781248734ca0
ph0b <xh@novetys.com>
2014-05-26 11:57:37 +0200

    make android-ndk build for all the supported archs
    setting APP_ABI:=all in Application.mk

--- Application.mk
+++ Application.mk
@@ -2,6 +2,6 @@
 NDK_TOOLCHAIN_VERSION := 4.8
 APP_PLATFORM          := android-19
 APP_STL               := gnustl_static
-APP_ABI               := armeabi-v7a # armeabi x86 mips
+APP_ABI               := all
 APP_BUILD_SCRIPT      := GypAndroid.mk
 APP_MODULES           := libmx3_gyp
