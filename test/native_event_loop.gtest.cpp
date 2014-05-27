5cf83f05f3b14e67d8bd47c906325d8c519b1538
Steven Kabbes <stevenkabbes@gmail.com>
2014-05-26 20:26:04 -0700

    Add another test for the c++ event loop

--- test/native_event_loop.gtest.cpp
+++ test/native_event_loop.gtest.cpp
@@ -1,5 +1,7 @@
 #include "stl.hpp"
 #include <atomic>
+#include <thread>
+#include <chrono>
 #include <gtest/gtest.h>
 
 #include <mx3/mx3.hpp>
@@ -7,6 +9,21 @@ using namespace mx3;
 
 TEST(NativeEventLoop, ctor_dtor) {
     NativeEventLoop loop;
+}
+
+TEST(NativeEventLoop, dtor_when_blocked) {
+    auto loop = make_unique<NativeEventLoop>();
+    semaphore ready_sem;
+    std::atomic_bool finished {false};
+    loop->post( [&] () {
+        ready_sem.notify();
+        std::this_thread::sleep_for( std::chrono::milliseconds(50) );
+        finished = true;
+    });
+    ready_sem.wait();
+    // call dtor by assigning to null
+    loop = nullptr;
+    EXPECT_EQ(finished, true);
 }
 
 TEST(NativeEventLoop, runs_functions) {
@@ -42,4 +59,23 @@ TEST(NativeEventLoop, runs_multi_functions) {
     ready_sem.wait();
     EXPECT_EQ(a_ran, true);
     EXPECT_EQ(b_ran, true);
+}
+
+TEST(NativeEventLoop, runs_nested_functions) {
+    // a slightly tricky example, because an implementation
+    // might accidentally hold the lock while calling the fn
+    NativeEventLoop loop;
+    std::atomic_int count{41};
+    semaphore ready_sem;
+    loop.post( [&] () {
+        loop.post( [&] () {
+            count++;
+            EXPECT_EQ(count, 43);
+            ready_sem.notify();
+        });
+        count++;
+        EXPECT_EQ(count, 42);
+    });
+    ready_sem.wait();
+    EXPECT_EQ(count, 43);
 }
