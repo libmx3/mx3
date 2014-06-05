37fb5cac80b0e83b9437283d00863fd47e0f8305
Steven Kabbes <stevenkabbes@gmail.com>
2014-06-05 10:36:19 -0700

    Remove objc github apis

--- example_ios/MX3/MX3/MXSampleDataTableViewController.m
+++ example_ios/MX3/MX3/MXSampleDataTableViewController.m
@@ -1,6 +1,5 @@
 #import "MXSampleDataTableViewController.h"
 #import "MX3Api+iOS.h"
-#import "MX3GithubAPI.h"
 
 NSString *const CellIdentifier = @"MX3Cell";
 
@@ -20,12 +19,6 @@ NSString *const CellIdentifier = @"MX3Cell";
     }];
     [self setupNavigationBar];
     [self registerCells];
-
-    [MX3GithubAPI getUsersWithSuccess:^(id JSON) {
-        NSLog(@"%@", JSON);
-    } failure:^(NSError *error) {
-        NSLog(@"%@", error);
-    }];
 }
 
 - (void)setupNavigationBar {
