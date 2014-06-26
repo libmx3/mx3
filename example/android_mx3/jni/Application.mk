1a5ff49186f6a5356bdfd8d43a82fbc42c32ed29
Gabriel Alvarez <gabeqaz@greatlate-lm.sanfrancisco.corp.yahoo.com>
2014-06-26 11:48:32 -0700

    Delete extra example folders

--- example/android_mx3/jni/Application.mk
+++
@@ -1,5 +0,0 @@
-NDK_TOOLCHAIN_VERSION := 4.8
-APP_PLATFORM          := android-19
-APP_STL               := gnustl_static
-APP_ABI               := armeabi-v7a armeabi x86
-APP_MODULES           := libmx3_jni
