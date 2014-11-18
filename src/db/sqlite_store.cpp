7803e23afc1eb81647ff9d683cde6c7137e2540b
Steven Kabbes <stevenkabbes@gmail.com>
2014-11-18 18:02:40 +0100

    Fix db schema change problem

--- src/db/sqlite_store.cpp
+++ src/db/sqlite_store.cpp
@@ -3,14 +3,15 @@
 using mx3::SqliteStore;
 using json11::Json;
 
-SqliteStore::SqliteStore(const string& db_path) {
+SqliteStore::SqliteStore(const string& db_path) : m_db()
+{
     m_db.open(db_path.c_str());
     _setup_db(m_db);
 }
 
 Json
 SqliteStore::get(const string& key) {
-    auto stmt = m_db.compileStatement("SELECT `value` FROM `kv` WHERE `key` = ?1");
+    auto stmt = m_db.compileStatement("SELECT `value` FROM `kv` WHERE `key` = ?1;");
     stmt.bind(1, key.c_str());
     auto query = stmt.execQuery();
 
@@ -27,16 +28,26 @@ void
 SqliteStore::set(const string& key, const Json& value) {
     auto serialized = value.dump();
 
-    auto stmt = m_db.compileStatement("INSERT INTO `kv` (`key`, `value`) VALUES (?1, ?2)");
+    bool did_update = false;
+    {
+    auto stmt = m_db.compileStatement("UPDATE `kv` SET `value` = ?2 WHERE `key` = ?1;");
     stmt.bind(1, key.c_str());
     stmt.bind(2, serialized.c_str());
-    stmt.execDML();
+    did_update = stmt.execDML() > 0;
+    }
+
+    if (!did_update) {
+        auto stmt = m_db.compileStatement("INSERT INTO `kv` (`key`, `value`) VALUES (?1, ?2);");
+        stmt.bind(1, key.c_str());
+        stmt.bind(2, serialized.c_str());
+        stmt.execDML();
+    }
 }
 
 void
 SqliteStore::_setup_db(CppSQLite3DB& db) {
     vector<string> setup_commands  {
-        "CREATE TABLE IF NOT EXISTS `kv` (`key` TEXT, `value` TEXT)"
+        "CREATE TABLE IF NOT EXISTS `kv` (`key` TEXT, `value` TEXT);"
     };
     for (const auto& cmd : setup_commands) {
         db.execDML(cmd.c_str());
