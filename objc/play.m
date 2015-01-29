5a4a5fee2870bcafc883b079e8554e7f92936bcf
Steven Kabbes <stevenkabbes@gmail.com>
2015-01-29 01:37:40 -0800

    Check for filepath before booting up API
    Fixes: https://github.com/libmx3/mx3/issues/41

--- objc/play.m
+++ objc/play.m
@@ -11,11 +11,17 @@ int main() {
     id <MX3Http> httpImpl = [[MX3HttpObjc alloc] init];
     id <MX3EventLoop> uiThread= [[MX3EventLoopObjc alloc] init];
     id <MX3ThreadLauncher> launcher = [[MX3ThreadLauncherObjc alloc] init];
-    id <MX3Api> api = [MX3ApiCppProxy createApi:@"../mx3" uiThread: uiThread httpImpl:httpImpl launcher:launcher];
-    if (![api hasUser]) {
-        [api setUsername: NSUserName()];
+    NSString *filePath = @"../mx3";
+    BOOL fileExists =[[NSFileManager defaultManager] fileExistsAtPath:filePath];
+    if (fileExists) {
+        id <MX3Api> api = [MX3ApiCppProxy createApi:filePath uiThread: uiThread httpImpl:httpImpl launcher:launcher];
+        if (![api hasUser]) {
+            [api setUsername: NSUserName()];
+        }
+        NSString * username = [api getUsername];
+        NSLog(@"Hello, %@", username);
+    } else {
+        NSLog(@"Could not find path: `%@`", filePath);
     }
-    NSString * username = [api getUsername];
-    NSLog(@"Hello, %@", username);
     return 0;
 }
